9fac7b573abe7156bfda2410d51bcc0c9cafe0c2
Jatin Garg <jtngrg1992@hotmail.com>
2022-04-11 22:09:11 +0530

    removed scene manifest, laid out the project

---
+++ VirualMandir/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VirualMandir
+//
+//  Created by Jatin Garg on 11/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+
+
+}
+
